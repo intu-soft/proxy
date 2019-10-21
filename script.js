041815d89dac549e5ecffdb05ff71b8843e53335
joondong2 <kwonjoondong@gmail.com>
2019-10-21 17:48:27 +0900

    키프리스 요청시 쿠키정보 삭제

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function searchKipris(queryString, kr) {
     }
 
     form.setAttribute('method', 'post');
-    form.setAttribute('Cookie', 'JSESSIONID=JtV3MmUbbCYZ8JVPkibxIfGrMEanqukZ4x7YDgELb1oa1VNUyWx5kqcKqctyDQdU.amV1c19kb21haW4va3BvcnRhbDQ=;');
+    //form.setAttribute('Cookie', 'JSESSIONID=JtV3MmUbbCYZ8JVPkibxIfGrMEanqukZ4x7YDgELb1oa1VNUyWx5kqcKqctyDQdU.amV1c19kb21haW4va3BvcnRhbDQ=;');
     if (!kr) {
         form.setAttribute('action', "http://abpat.kipris.or.kr/abpat/resulta.do");
     } else {
